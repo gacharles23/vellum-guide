e6b23628d6b77ae451749e8da77c844bcb5e08c0
Grace <123527324+gacharles23@users.noreply.github.com>
2026-08-14 20:25:13 -0400

    Create Byline.vue

---
+++ docs/.vitepress/theme/Byline.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { useData } from 'vitepress'
+const { frontmatter } = useData()
+</script>
+
+<template>
+  <div class="vg-byline" v-if="frontmatter.authors?.length || frontmatter.version">
+    <span>Written by the Vellum community</span>
+    <template v-if="frontmatter.authors?.length">
+      <span class="vg-dot">·</span>
+      <span class="vg-names">{{ frontmatter.authors.join(', ') }}</span>
+    </template>
+    <template v-if="frontmatter.version">
+      <span class="vg-dot">·</span>
+      <span class="vg-chip">checked against {{ frontmatter.version }}</span>
+    </template>
+  </div>
+</template>
